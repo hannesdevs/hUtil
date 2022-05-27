3678f811bbc665ef2d8c863115a257abe1855dcb
hannesdevs <johannes.bingen@gmail.com>
2022-05-27 13:00:57 +0200

    bug fixes
    had to replace interactionCreate, and fix the errors that i got :)

--- Events/Client/interactionCreate.js
+++ Events/Client/interactionCreate.js
@@ -3,20 +3,17 @@ const {
     CommandInteraction,
     MessageEmbed
 } = require("discord.js");
-const {
-    execute
-} = require("../ready");
 
 module.exports = {
-    name: "InteractionCreate",
+    name: "interactionCreate",
     /**
-     * 
      * @param {CommandInteraction} interaction 
      * @param {Client} client 
      */
     async execute(interaction, client) {
         if (interaction.isCommand()) {
             const command = client.commands.get(interaction.commandName);
+
             if (!command) return interaction.reply({
                 embeds: [
                     new MessageEmbed()
@@ -24,13 +21,15 @@ module.exports = {
                     .setDescription("❌ An error occurred while running this command.")
                 ]
             }) && client.commands.delete(interaction.commandName);
+
+            if (command.permission && !interaction.member.permissions.has(command.permission)) {
+                return interaction.reply({
+                    content: `You do not have the required permission for this command: \`${interaction.commandName}\`.`,
+                    ephemeral: true
+                })
+            };
+
+            command.execute(interaction, client);
         }
-        if (command.permission && !interaction.member.permissions.has(command.permission)) {
-            return interaction.reply({
-                content: `You do not have the required permission for this command: \`${interaction.commandName}\`.`,
-                ephemeral: true
-            })
-        }
-        commands.execute(interaction, client)
     }
 }
